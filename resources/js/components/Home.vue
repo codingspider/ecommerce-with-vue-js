2f12972eb5d910ad9a8b12450f36d909d61f7aca
codingspider <engrokon.rok@gmail.com>
2020-05-11 02:27:31 +0600

    updated

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <p>This is Home Page </p>
+      <p> {{ welcome }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'home',
+   computed: {
+     welcome(){
+       return this.$store.getters.welcome
+     }
+   }
+}
+</script>
+
